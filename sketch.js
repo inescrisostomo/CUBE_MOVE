7b3d7464a82d36dd2f77f5c3ba40107ef02cff0f
inescrisostomo <96204642+inescrisostomo@users.noreply.github.com>
2022-01-26 16:01:09 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let x = 100;
+
+
+function setup() {
+  createCanvas(windowWidth, windowWidth, WEBGL);
+}
+
+function draw() {
+  color(255);
+
+  rotateX(frameCount * 0.3);
+  rotateY(frameCount * 0.1);
+  box(x,50);
+  
+  x = x+3;
+  
+}
